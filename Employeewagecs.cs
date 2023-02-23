1399c7c8b67f834ffb0901e5f830f8ddd4916a93
saidatla201 <saidatla201@gmail.com>
2023-02-23 21:29:52 +0530

    Calculate Daily Wage

--- Employeewagecs.cs
+++ Employeewagecs.cs
@@ -8,20 +8,31 @@ namespace EmpWageUsingOops
 {
     public class Employeewagecs
     {
+        
         public void CheckEmployee()
         {
             const int IS_PRESENT = 1;
+            const int EMP_RATE_PER_HR = 20;
+
+            int empHrs = 0, empWage = 0;
+
             Random random = new Random();
             int employeeCheck = random.Next(2);
 
             if (employeeCheck == IS_PRESENT)
             {
                 Console.WriteLine("Employee is present");
+                empHrs = 8;
             }
             else
             {
                 Console.WriteLine("Employee is absent");
+                empHrs = 0;
             }
+
+            empWage = empHrs * EMP_RATE_PER_HR;
+            Console.WriteLine("Employee daily wage is : " + empWage);
+
         }
     }
 }

07ea1320003057c224aef784201e235d5ec40ce4
saidatla201 <saidatla201@gmail.com>
2023-02-23 21:19:44 +0530

    Welcome message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmpWageUsingOops
+{
+    public  class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Employee Wage By using Oops");
+        }
+    }
+}

3def7cad14d7a7a4e52d140c553416ab59513742
saidatla201 <saidatla201@gmail.com>
2023-02-23 21:47:48 +0530

    Refactoring the code

--- Employeewagecs.cs
+++ Employeewagecs.cs
@@ -8,14 +8,13 @@ namespace EmpWageUsingOops
 {
     public class Employeewagecs
     {
+        const int IS_FULL_TIME = 1;
+        const int IS_PART_TIME = 2;
+        const int EMP_RATE_PER_HR = 20;
+        const int WORKING_DAYS = 20;
+        const int MAX_WOKING_HRS = 100;
         public void CheckEmployee()
         {
-            const int IS_FULL_TIME = 1;
-            const int IS_PART_TIME = 2;
-            const int EMP_RATE_PER_HR = 20;
-            const int WORKING_DAYS = 20;
-            const int MAX_WOKING_HRS = 100;
-
             int empHrs = 0, empWage = 0, totalEmpWage = 0, totalEmpHrs = 0, totalWokingDays = 0;
 
             while (totalWokingDays < WORKING_DAYS && totalEmpHrs <= MAX_WOKING_HRS)

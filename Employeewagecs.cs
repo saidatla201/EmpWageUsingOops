13e971eb93e9c115bdbaae21fd94d73736b7419d
saidatla201 <saidatla201@gmail.com>
2023-02-23 21:45:19 +0530

    Empwage upto maximum 100 hours in a month

--- Employeewagecs.cs
+++ Employeewagecs.cs
@@ -8,17 +8,17 @@ namespace EmpWageUsingOops
 {
     public class Employeewagecs
     {
-
         public void CheckEmployee()
         {
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
             const int WORKING_DAYS = 20;
+            const int MAX_WOKING_HRS = 100;
 
-            int empHrs = 0, empWage = 0, totalEmpWage = 0;
+            int empHrs = 0, empWage = 0, totalEmpWage = 0, totalEmpHrs = 0, totalWokingDays = 0;
 
-            for (int day = 0; day < WORKING_DAYS; day++)
+            while (totalWokingDays < WORKING_DAYS && totalEmpHrs <= MAX_WOKING_HRS)
             {
 
                 Random random = new Random();
@@ -43,9 +43,14 @@ namespace EmpWageUsingOops
 
                 empWage = empHrs * EMP_RATE_PER_HR;
                 totalEmpWage = totalEmpWage + empWage;
-                Console.WriteLine("day {0} employee wage is :{1} ", day, empWage);
+                totalEmpHrs = totalEmpHrs + empHrs;
+
+                totalWokingDays++;
+
             }
-            Console.WriteLine("total wage: " + totalEmpWage);
+            Console.WriteLine("total wage for {0} days :{1} ", totalWokingDays, totalEmpWage);
+            Console.WriteLine("total working hours: " + totalEmpHrs);
         }
+
     }
 }

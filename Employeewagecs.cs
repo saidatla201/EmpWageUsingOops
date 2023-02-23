7713f5b7686e0930e377ee7ed3263afcf1494e73
saidatla201 <saidatla201@gmail.com>
2023-02-23 21:37:18 +0530

    Checking is Employee is PartTime or Fulltime

--- Employeewagecs.cs
+++ Employeewagecs.cs
@@ -8,21 +8,27 @@ namespace EmpWageUsingOops
 {
     public class Employeewagecs
     {
-        
+
         public void CheckEmployee()
         {
-            const int IS_PRESENT = 1;
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
 
             int empHrs = 0, empWage = 0;
 
             Random random = new Random();
-            int employeeCheck = random.Next(2);
+            int employeeCheck = random.Next(3);
 
-            if (employeeCheck == IS_PRESENT)
+            if (employeeCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is present");
+                Console.WriteLine("Full Time Employee is present");
                 empHrs = 8;
+            }
+            else if (employeeCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("PartTime Employee is present");
+                empHrs = 4;
             }
             else
             {
@@ -31,7 +37,7 @@ namespace EmpWageUsingOops
             }
 
             empWage = empHrs * EMP_RATE_PER_HR;
-            Console.WriteLine("Employee daily wage is : " + empWage);
+            Console.WriteLine("Employee wage is : " + empWage); 
 
         }
     }

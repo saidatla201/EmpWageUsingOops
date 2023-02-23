3ebea03757e80cbf9d5a3ee2f6677396a2098e7a
saidatla201 <saidatla201@gmail.com>
2023-02-23 21:26:15 +0530

    Check employee is Present or Absent

---
+++ Employeewagecs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageUsingOops
+{
+    public class Employeewagecs
+    {
+        public void CheckEmployee()
+        {
+            const int IS_PRESENT = 1;
+            Random random = new Random();
+            int employeeCheck = random.Next(2);
+
+            if (employeeCheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
